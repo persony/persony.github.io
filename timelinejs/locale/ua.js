86ecfd6cf0ff239c1ceb9a49e7c6384f774a1e03
test <test@debian>
2014-01-26 19:00:13 +0200

    Finish layout

---
+++ timelinejs/locale/ua.js
@@ -0,0 +1,10 @@
+/*
+    TimelineJS - ver. 2014-01-10-19-05-31 - 2014-01-10
+    Copyright (c) 2012-2013 Northwestern University
+    a project of the Northwestern University Knight Lab, originally created by Zach Wise
+    https://github.com/NUKnightLab/TimelineJS
+    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+    If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+/* Ukrainian LANGUAGE 
+ ================================================== */typeof VMM!="undefined"&&(VMM.Language={lang:"ua",api:{wikipedia:"ua"},date:{month:["Січень","Лютий","Березень","Квітень","Травень","Червень","Липень","Серпень","Вересень","Жовтень","Листопад","Грудень"],month_abbr:["Січ.","Лют.","Бер.","Кві.","Тра.","Чер.","Лип.","Сер.","Вер.","Жов.","Лис.","Гру."],day:["Неділя","Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота"],day_abbr:["Нд.","Пн.","Вт.","Ср.","Чт.","Пт.","Сб."]},dateformats:{year:"yyyy",month_short:"mmm",month:"mmmm yyyy",full_short:"mmm d",full:"mmmm d',' yyyy",time_short:"h:MM:ss TT",time_no_seconds_short:"h:MM TT",time_no_seconds_small_date:"h:MM TT'<br/><small>'mmmm d',' yyyy'</small>'",full_long:"mmm d',' yyyy 'at' hh:MM TT",full_long_small_date:"hh:MM TT'<br/><small>mmm d',' yyyy'</small>'"},messages:{loading_timeline:"Завантажується... ",return_to_title:"Повернутись до першої події",expand_timeline:"Збільшити",contract_timeline:"Зменшити",wikipedia:"З Wikipedia",loading_content:"Завантаження матеріалів",loading:"Завантаження"}});
